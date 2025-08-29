0f3b419ba501e475d4f24a6226428fff22a1786b
Ray <ruizhaoma@gmail.com>
2025-08-29 10:33:55 -0700

    feat: implement solution for Largest Rectangle In Histogram

---
+++ leetcode/03_stack/07_largest_rectangle_in_histogram.py
@@ -0,0 +1,54 @@
+'''
+Given an array of integers heights representing the histogram's bar height where the width of each bar is 1, return the area of the largest rectangle in the histogram.
+
+ 
+
+Example 1:
+
+
+Input: heights = [2,1,5,6,2,3]
+Output: 10
+Explanation: The above is a histogram where width of each bar is 1.
+The largest rectangle is shown in the red area, which has an area = 10 units.
+Example 2:
+
+
+Input: heights = [2,4]
+Output: 4
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+0 <= heights[i] <= 104
+'''
+
+
+from typing import List
+
+class Solution:
+    @staticmethod
+    def largestRectangleArea(heights: List[int]) -> int:
+        stack_height_index = []
+        max_area = 0
+
+        for i, height in enumerate(heights):
+            width_start = i
+            while stack_height_index and stack_height_index[-1][0] > height:
+                popped_height, width_start = stack_height_index.pop()
+                width = i - width_start
+                area = width * popped_height
+                max_area = max(max_area, area)
+            stack_height_index.append((height, width_start))
+
+        n = len(heights)
+
+        while stack_height_index:
+            popped_height, width_start = stack_height_index.pop()
+            width = n - width_start
+            area = width * popped_height
+            max_area = max(max_area, area)
+
+        return max_area
+
+print(Solution.largestRectangleArea(heights=[2,1,5,6,2,3]))
